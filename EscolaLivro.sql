fbbeff194747fc6cf99dc8136844017e24116e85
Martins <99661044+LostRoses@users.noreply.github.com>
2024-05-01 15:14:21 -0300

    Update EscolaLivro.sql

--- EscolaLivro.sql
+++ EscolaLivro.sql
@@ -23,33 +23,20 @@ CREATE TABLE Livro (
     titulo VARCHAR(255),
     autor VARCHAR(255),
     qnt_em_estoque INT
-);
-
+);	
 CREATE TABLE Emprestimo (
     id_emprestimo INT PRIMARY KEY,
     id_aluno INT,
+	id_curso INT,
     id_livro INT,
     data_emprestimo DATE,
     data_devolucao DATE,
     data_devolvida DATE,
     multa VARCHAR (255),
     FOREIGN KEY (id_aluno) REFERENCES Aluno(id_aluno),
-    FOREIGN KEY (id_livro) REFERENCES Livro(id_livro)
-);
-
-CREATE TABLE Cursos (
-    id_cursos INT PRIMARY KEY,
-    nome_dos_cursos VARCHAR(255)
-);
-
-CREATE TABLE Cadeira (
-    id_cadeira INT PRIMARY KEY,
-    id_curso INT,
-    nome_cadeira VARCHAR (255),
-    preco_cadeira VARCHAR (255),
-    FOREIGN KEY (id_curso) REFERENCES Curso (id_curso)
-);
-
+    FOREIGN KEY (id_livro) REFERENCES Livro(id_livro),
+	FOREIGN KEY (id_curso) REFERENCES Curso(id_curso)
+);	
 CREATE TABLE Professor (
     id_professor INT PRIMARY KEY,
     id_curso INT,
@@ -110,57 +97,40 @@ DELETE FROM Curso WHERE id_curso = 9;
 -- Tabela Aluno
 
 INSERT INTO Aluno VALUES (1, 1, 'João Martins', 'joao.00000829607@unicap.br', '2002-03-04', 'Rua Subida do Perijucan, 171'); 
-INSERT INTO Aluno VALUES (2, 5, 'Maria Santos', 'maria.santos@gmail.com', '1999-02-02', 'Avenida das Árvores, 456'); 
-INSERT INTO Aluno VALUES (3, 2, 'Pedro Costa', 'pedro.costa@gmail.com', '2001-03-03', 'Travessa do Sol, 789'); 
-INSERT INTO Aluno VALUES (4, 3, 'Ana Pereira', 'ana.pereira@gmail.com', '2002-04-04', 'Beco da Lua, 012'); 
-INSERT INTO Aluno VALUES (5, 4, 'Luiz Oliveira', 'luiz.oliveira@gmail.com', '2003-05-05', 'Praça das Estrelas, 345'); 
+INSERT INTO Aluno VALUES (2, 2, 'Maria Santos', 'maria.santos@gmail.com', '1999-02-02', 'Avenida das Árvores, 456'); 
+INSERT INTO Aluno VALUES (3, 3, 'Pedro Costa', 'pedro.costa@gmail.com', '2001-03-03', 'Travessa do Sol, 789'); 
+INSERT INTO Aluno VALUES (4, 4, 'Ana Pereira', 'ana.pereira@gmail.com', '2002-04-04', 'Beco da Lua, 012'); 
+INSERT INTO Aluno VALUES (5, 5, 'Luiz Oliveira', 'luiz.oliveira@gmail.com', '2003-05-05', 'Praça das Estrelas, 345'); 
 UPDATE Aluno SET nome = 'Maria Santos', email = 'marisantos@gmail.com' WHERE id_aluno = 2;
 
 -- Tabela Livro
 
-INSERT INTO Livro VALUES (1, 'O Pequeno Príncipe', 'Antoine de Saint-Exupéry', 10); 
-INSERT INTO Livro VALUES (2, 'Dom Quixote', 'Miguel de Cervantes', 5); 
-INSERT INTO Livro VALUES (3, '1984', 'George Orwell', 8); 
-INSERT INTO Livro VALUES (4, 'A Revolução dos Bichos', 'George Orwell', 12); 
-INSERT INTO Livro VALUES (5, 'Cem Anos de Solidão', 'Gabriel García Márquez', 6);
+--História
+INSERT INTO Livro VALUES (1, 'A Formação da Classe Operária Inglesa', 'E. P. Thompson', 10); 
+INSERT INTO Livro VALUES (2, 'Casa-Grande & Senzala', 'Gilberto Freyr', 25); 
+INSERT INTO Livro VALUES (3, 'História geral do Brasil', 'Ciro Flamarion Cardoso', 18); 
 
-UPDATE Livro SET id_livro = '2', titulo = 'As coisas que você só vê quando você desacelera', autor = 'Haemin Sunim', qnt_em_estoque = '19' WHERE id_livro = 2;
+--Ciência da Computação
+INSERT INTO Livro VALUES (4, 'Código Limpo', 'Robert Cecil Martin', 12); 
+INSERT INTO Livro VALUES (5, 'Use A Cabeça Java', 'Kathy Sierra', 16);
+INSERT INTO Livro VALUES (6, 'Data Science Do Zero', 'Joel Grus', 10); 
 
--- Tabela Cursos
+--Direito
+INSERT INTO Livro VALUES (7, 'Teoria Pura do Direito', 'Hans Kelsen', 15); 
+INSERT INTO Livro VALUES (8, 'Justiça - O que é fazer a coisa certa?', 'Michael J. Sandel', 18); 
+INSERT INTO Livro VALUES (9, 'Introdução ao estudo do direito', 'Paulo Nader', 15); 
 
-INSERT INTO Cursos VALUES (1, 'Ciência da Computação');
-INSERT INTO Cursos VALUES (2, 'Arquitetura');
-INSERT INTO Cursos VALUES (3, 'Jornalismo ');
-INSERT INTO Cursos VALUES (4, 'Medicina ');
-INSERT INTO Cursos VALUES (5, 'Direito');
+-- Medicina
+INSERT INTO Livro VALUES (10, 'Moore-Anatomia Orientada Para a Clinica', ' Arthur F. Dalley II', 16);
+INSERT INTO Livro VALUES (11, 'Microbiologia Médica e Imunologia', 'Ernest Jawetz e Warren E. Levinson', 13); 
+INSERT INTO Livro VALUES (12, 'Langman Embriologia Médica', 'T. W. Sadler', 15); 
 
--- Tabela Cadeira
+--letras
+INSERT INTO Livro VALUES (13, 'Teoria da literatura: uma introdução', 'Terry Eagleton', 14); 
+INSERT INTO Livro VALUES (14, 'Os gêneros do discurso ', 'Mikhail Bakhtin', 12); 
+INSERT INTO Livro VALUES (15, 'Produção textual, análise de gêneros e compreensão', 'Luiz Antônio Marcuschi', 16);
 
--- CIENCIA DA COMPUTAÇÃO
-INSERT INTO Cadeira VALUES (1, 1, 'Introdução a Ciência da Computação','R$: 250,00');
-INSERT INTO Cadeira VALUES (2, 1, 'Eletronica basica', 'R$: 200,00');
-INSERT INTO Cadeira VALUES (3, 1, 'Gráfos', 'R$: 250,00');
-
--- LETRAS
-INSERT INTO Cadeira VALUES (4, 1, 'Gramatica 1','R$: 250,00');
-INSERT INTO Cadeira VALUES (5, 1, 'Argumentação e Disertação', 'R$: 200,00');
-INSERT INTO Cadeira VALUES (6, 1, 'Oratoria', 'R$: 250,00');
-
---HISTORIA Geral
-INSERT INTO Cadeira VALUES (7, 1, 'Introdução a História Geral','R$: 250,00');
-INSERT INTO Cadeira VALUES (8, 1, 'Guerras', 'R$: 200,00');
-INSERT INTO Cadeira VALUES (9, 1, 'História Hoje', 'R$: 250,00');
-
-
--- MEDICINA
-INSERT INTO Cadeira VALUES (10, 1, 'Introdução ao Corpo Humano','R$: 250,00');
-INSERT INTO Cadeira VALUES (11, 1, 'Sangue', 'R$: 200,00');
-INSERT INTO Cadeira VALUES (12, 1, 'Fraturas', 'R$: 250,00');
-
--- DIREITO
-INSERT INTO Cadeira VALUES (13, 1, 'Introdução a Direito','R$: 250,00');
-INSERT INTO Cadeira VALUES (14, 1, 'Código Penal', 'R$: 200,00');
-INSERT INTO Cadeira VALUES (15, 1, 'Direito do Consumidor', 'R$: 250,00');
+UPDATE Livro SET id_livro = '2', titulo = 'Era do Capital', autor = 'Eric Hobsbawm', qnt_em_estoque = '19' WHERE id_livro = 2;
 
 
 --Tabela Professor
@@ -181,21 +151,20 @@ INSERT INTO Inscricoes VALUES (5, 4, 3, '2024-01-04');
 INSERT INTO Inscricoes VALUES (6, 5, 5, '2024-01-03');
 
 --Tabela Emprestimo
-
-INSERT INTO Emprestimo VALUES (1,1,1, '2024-03-10', '2024-03-25','2024-03-20');
-INSERT INTO Emprestimo VALUES (2,1,3, '2024-04-03', '2024-05-18','2024-05-18');
-INSERT INTO Emprestimo VALUES (3,2,4, '2024-02-11','2024-02-26','2024-02-25');
-INSERT INTO Emprestimo VALUES (4,3,5, '2024-03-07','2024-03-22','2024-03-25', ' R$: 15,00');
-INSERT INTO Emprestimo VALUES (5,4,2, '2024-03-14','2024-03-29','2024-03-25');
-INSERT INTO Emprestimo VALUES (6,5,1, '2024-04-08','2024-04-23','2024-04-25',' 10,00');
+-- id_emprestimo; id_curso,id_aluno, id_livro
+INSERT INTO Emprestimo VALUES (1,1,1,4, '2024-03-10', '2024-03-25','2024-03-20');
+INSERT INTO Emprestimo VALUES (2,2,2,13, '2024-04-03', '2024-05-18','2024-05-18');
+INSERT INTO Emprestimo VALUES (3,3,3,1, '2024-02-11','2024-02-26','2024-02-25');
+INSERT INTO Emprestimo VALUES (4,4,4,11 ,'2024-03-07','2024-03-22','2024-03-25', ' R$: 15,00');
+INSERT INTO Emprestimo VALUES (5,5,5,7 ,'2024-03-14','2024-03-29','2024-03-25');
 
 --Tabela Avaliacao
-
-INSERT INTO Avaliacao VALUES (1,'João Martins', 1,1, 5,'10', 5, 1,'10');
-INSERT INTO Avaliacao VALUES (2,'Maria Santos', 5,2, 2,'9', 2, 2,'10');
-INSERT INTO Avaliacao VALUES (3,'Pedro Costa', 2,3, 3,'7', 3, 3,'8');
-INSERT INTO Avaliacao VALUES (4,'Ana Pereira', 3,4,4 ,'6', 4, 4,'6,5');
-INSERT INTO Avaliacao VALUES (5,'Luiz Oliveira', 4,5, 1,'7', 5, 5,'7,75');
+-- id_avaliacao,nome_aluno,id_aluno ,id_curso,id_professor ,nota,id_bonus ,nota_atualizada ,
+INSERT INTO Avaliacao VALUES (1,'João Martins', 1,1, 5,'10', 1, '10');
+INSERT INTO Avaliacao VALUES (2,'Maria Santos', 5,2, 2,'9', 2, '10');
+INSERT INTO Avaliacao VALUES (3,'Pedro Costa', 2,3, 3,'7', 3, '8');
+INSERT INTO Avaliacao VALUES (4,'Ana Pereira', 3,4,4 ,'6', 4,'6,5');
+INSERT INTO Avaliacao VALUES (5,'Luiz Oliveira', 4,5, 1,'7', 5,'7,75');
 
 --Tabela Bonus
 
@@ -222,14 +191,35 @@ JOIN Inscricoes ON Curso.id_curso = Inscricoes.id_curso
 GROUP BY Curso.nome_do_curso
 ORDER BY quantidade_inscricoes DESC;
 
-SELECT Aluno.nome, SUM(Bonus.pontos_bonus) AS total_pontos_bonus
-FROM Aluno
-JOIN Bonus ON Aluno.id_aluno = Bonus.id_aluno
-GROUP BY Aluno.nome
-ORDER BY total_pontos_bonus DESC;
-
-SELECT Curso.nome_curso, MAX(Avaliacao.nota) - MIN(Avaliacao.nota) AS diferenca_notas
+SELECT Curso.nome_do_curso, COUNT(Aluno.id_aluno) AS qnt_alunos_com_bonus_acima_media
 FROM Curso
-JOIN Avaliacao ON Curso.id_curso = Avaliacao.id_curso
-GROUP BY Curso.nome_curso;
+JOIN Aluno ON Curso.id_curso = Aluno.id_curso
+JOIN Bonus ON Aluno.id_aluno = Bonus.id_aluno
+WHERE Bonus.pontos_bonus > (SELECT AVG(pontos_bonus) FROM Bonus)
+GROUP BY Curso.nome_do_curso;
 
+SELECT AVG(num_emprestimos) as media_emprestimos
+FROM (SELECT id_aluno, COUNT(*) as num_emprestimos
+      FROM Emprestimo
+      GROUP BY id_aluno) as subquery;
+	  
+SELECT COUNT(*) as emprestimos_atrasados
+FROM Emprestimo
+WHERE data_devolvida > data_devolucao;
+
+SELECT COUNT(*) AS 'Número de alunos com média abaixo de 7'
+FROM Avaliacao
+WHERE nota < 7;
+
+CREATE OR REPLACE FUNCTION update_bonus_points() RETURNS TRIGGER AS $$
+BEGIN
+    IF (NEW.id_livro BETWEEN (NEW.id_curso-1)*3+1 AND NEW.id_curso*3) THEN
+        UPDATE Bonus SET pontos_bonus = pontos_bonus + 1000 WHERE id_aluno = NEW.id_aluno;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER bonus_points_after_insert
+AFTER INSERT ON Emprestimo
+FOR EACH ROW EXECUTE PROCEDURE update_bonus_points();
